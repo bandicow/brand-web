41ce35569d723837fe00a9fe6f8f68d786187fc3
JUN HYEOK KIM <kjh45388117@gmail.com>
2023-05-17 21:37:08 +0900

    make-by-next

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+// const withSass = require("@zeit/next-sass");
+
+// module.exports = withSass({
+//   /* 추가적인 옵션 설정 가능 */
+// });
